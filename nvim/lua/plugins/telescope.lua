5ea5eec4048cfcb0450146d9261f16b284a14fcd
Samdan Dansranbavuu <samdan.dev@gmail.com>
2023-09-03 21:12:59 +0800

    initial config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,54 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      -- configure to use ripgrep
+      vimgrep_arguments = {
+        "rg",
+        "--follow", -- Follow symbolic links
+        "--hidden", -- Search for hidden files
+        "--no-heading", -- Don't group matches by each file
+        "--with-filename", -- Print the file path with the matched lines
+        "--line-number", -- Show line numbers
+        "--column", -- Show column numbers
+        "--smart-case", -- Smart case search
+
+        -- Exclude some patterns from search
+        "--glob=!**/.git*",
+        "--glob=!**/node_modules*",
+        "--glob=!**/.idea*",
+        "--glob=!**/.vscode*",
+        "--glob=!**/build*",
+        "--glob=!**/dist*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
+      file_ignore_patterns = {
+        "node_modules",
+        "build",
+        "dist",
+        "yarn.lock",
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+        -- needed to exclude some files & dirs from general search
+        -- when not included or specified in .gitignore
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "--glob=!**/.git*",
+          "--glob=!**/node_modules*",
+          "--glob=!**/.idea*",
+          "--glob=!**/.vscode*",
+          "--glob=!**/build*",
+          "--glob=!**/dist*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+        },
+      },
+    },
+  },
+}
