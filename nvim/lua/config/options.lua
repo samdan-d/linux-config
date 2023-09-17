887a23d34237c315365be8cc002f94e54ae618db
Samdan Dansranbavuu <samdan.dev@gmail.com>
2023-09-17 19:26:38 +0800

    modified

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,8 +4,16 @@
 
 local opt = vim.opt
 opt.winbar = "%=%m %f"
-
+opt.termguicolors = true
 opt.hlsearch = false
 opt.incsearch = true
 opt.clipboard = "unnamed"
 opt.list = false
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect"
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
